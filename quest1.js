db1870fc5dfc14ded6dbbc4ee4c7e2b6e3ede9b7
Lanna-Maria <lannamarya2016@gmail.com>
2025-06-01 21:41:39 -0300

    Adicionando questão 1

---
+++ quest1.js
@@ -0,0 +1,7 @@
+// 1. Crie uma função que recebe dois números e retorne o maior dos dois.
+
+function maiorNumero(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
+
+console.log(maiorNumero(10, 20)); // 20
